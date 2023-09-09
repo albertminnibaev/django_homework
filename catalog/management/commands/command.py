3218d58224c1f6c157a119b6fbd6b5d4d7cfc72a
Albert <albert.minnibaeff@yandex.ru>
2023-09-09 18:35:31 +0400

    I've finished my homework 20.1

--- catalog/management/commands/command.py
+++ catalog/management/commands/command.py
@@ -10,7 +10,8 @@ class Command(BaseCommand):
 
         #Category.objects.all().delete()
 
-        Category.objects.raw('TRUNCATE TABLE category RESTART IDENTITY;')
+        with connection.cursor() as cursor:
+            cursor.execute('TRUNCATE TABLE catalog_category RESTART IDENTITY CASCADE')
 
         category_list = [
             {'name': 'фрукты', 'descriptions': 'вкусные'},
@@ -23,10 +24,10 @@ class Command(BaseCommand):
             Category.objects.create(**category_item)
 
         product_list = [
-            {'name': 'апельсин', 'descriptions': 'оранжевый', 'category': 1, 'price': 100},
-            {'name': 'огурец', 'descriptions': 'зеленый', 'category': 2, 'price': 30},
-            {'name': 'малина', 'descriptions': 'любят дети', 'category': 3, 'price': 200},
-            {'name': 'миндаль', 'descriptions': 'вкусный', 'category': 4, 'price': 300}
+            {'name': 'апельсин', 'descriptions': 'оранжевый', 'category': Category.objects.get(pk=1), 'price': 100},
+            {'name': 'огурец', 'descriptions': 'зеленый', 'category': Category.objects.get(pk=2), 'price': 30},
+            {'name': 'малина', 'descriptions': 'любят дети', 'category': Category.objects.get(pk=3), 'price': 200},
+            {'name': 'миндаль', 'descriptions': 'вкусный', 'category': Category.objects.get(pk=4), 'price': 300}
         ]
 
         for product_item in product_list:
