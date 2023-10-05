25d64e147d22285359bcf54f422a370407442212
Albert <albert.minnibaeff@yandex.ru>
2023-10-06 00:20:17 +0400

    completed task * homework 23.2

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from blog.apps import BlogConfig
 from blog.views import RecordCreateView, RecordListView, RecordDetailView, RecordUpdateView, RecordDeleteView
@@ -6,15 +7,9 @@ from blog.views import RecordCreateView, RecordListView, RecordDetailView, Recor
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('create/', RecordCreateView.as_view(), name='create'),
+    path('create/', never_cache(RecordCreateView.as_view()), name='create'),
     path('', RecordListView.as_view(), name='list'),
     path('view/<int:pk>/', RecordDetailView.as_view(), name='view'),
-    path('edit/<int:pk>/', RecordUpdateView.as_view(), name='edit'),
+    path('edit/<int:pk>/', never_cache(RecordUpdateView.as_view()), name='edit'),
     path('delete/<int:pk>/', RecordDeleteView.as_view(), name='delete'),
-    # path('categories/', CategoryListView.as_view(), name='categories'),
-    # path('products/<int:pk>/', ProductListView.as_view(), name='products'),
-    # path('products/create/', ProductCreateView.as_view(), name='product_create'),
-    # path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
-    # path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
-    # path('contacts/', contact, name='contacts'),
 ]
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
 from catalog.views import CategoryListView, ProductListView, ProductCreateView, ProductUpdateView, \
@@ -13,13 +13,13 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('products/<int:pk>/', ProductListView.as_view(), name='products'),
-    path('products/create/', ProductCreateView.as_view(), name='product_create'),
-    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/create/', never_cache(ProductCreateView.as_view()), name='product_create'),
+    path('products/update/<int:pk>/', never_cache(ProductUpdateView.as_view()), name='product_update'),
     path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
-    path('products/edit/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_edit'),
+    path('products/edit/<int:pk>/', ProductDetailView.as_view(), name='product_edit'),
     path('version/<int:pk>/', VersionListView.as_view(), name='version'),
-    path('version/create/', VersionCreateView.as_view(), name='version_create'),
-    path('version/update/<int:pk>/', VersionUpdateView.as_view(), name='version_update'),
+    path('version/create/', never_cache(VersionCreateView.as_view()), name='version_create'),
+    path('version/update/<int:pk>/', never_cache(VersionUpdateView.as_view()), name='version_update'),
     path('version/delete/<int:pk>/', VersionDeleteView.as_view(), name='version_delete'),
     path('version/edit/<int:pk>/', VersionDetailView.as_view(), name='version_edit'),
     path('contacts/', contact, name='contacts'),
